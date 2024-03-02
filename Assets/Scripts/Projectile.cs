a4bceec7559adb0b32cedd0d42933d594b448f13
maxatroshenko <maxatr@yandex.ru>
2024-03-02 15:50:55 -0500

    Range Tank workflow

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -5,6 +6,17 @@ public class Projectile : MonoBehaviour
     [SerializeField] private int damage = 5;
     [SerializeField] private float speed = 5f;
     [SerializeField] private string myTag = "";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var tank = collision.GetComponent<Tank>();
+
+        if (tank != null && collision.gameObject.tag != myTag)
+        {
+            tank.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
 
     private void Update()
     {
---
+++ Assets/Scripts/RangeTank.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeTank : ShootableTank
+{
+    [SerializeField] private float distanceToPlayer = 5f;
+    private float _timer;
+    private Transform _target;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    protected override void Move()
+    {
+        transform.Translate(Vector2.down * (movementSpeed * Time.deltaTime));
+    }
+
+    protected void Update()
+    {
+        if (Vector2.Distance(transform.position, _target.position) > distanceToPlayer)
+        {
+            Move();
+        }
+
+        SetAngle(_target.position);
+        
+        if (_timer <= 0)
+        {
+            Shoot();
+
+            _timer = reloadTime;
+        }
+        else
+        {
+            _timer -= Time.deltaTime;
+        }
+    }
+}
