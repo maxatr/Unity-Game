12f1494fa2e57113ff2987c75f358caf091ee01f
maxatroshenko <maxatr@yandex.ru>
2024-03-06 21:52:40 -0500

    Editor

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,6 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    
+    [System.Serializable]
+    public class Item
+    {
+        public int Id;
+        public string Name;
+        public Sprite Image;
+    }
+
+    public List<Item> Items;
 }
--- Assets/Scripts/InventoryEditor.cs
+++ Assets/Scripts/InventoryEditor.cs
@@ -1,6 +1,39 @@
+using UnityEditor;
 using UnityEngine;
 
-public class InventoryEditor : MonoBehaviour
+[CustomEditor(typeof(Inventory))]
+public class InventoryEditor : Editor
 {
+    private Inventory _inventory;
     
+    public void OnEnable()
+    {
+        _inventory = (Inventory) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (_inventory.Items.Count > 0)
+        {
+            foreach (var item in _inventory.Items)
+            {
+                EditorGUILayout.BeginVertical("box");
+                
+                item.Id = EditorGUILayout.IntField("ID", item.Id);
+                item.Name = EditorGUILayout.TextField("Name", item.Name);
+                item.Image = (Sprite) EditorGUILayout.ObjectField("Image", item.Image, typeof(Sprite), false);
+                
+                EditorGUILayout.EndVertical();
+            }
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Inventory is empty");
+        }
+
+        if (GUILayout.Button("Add item", GUILayout.Width(200), GUILayout.Height(30)))
+        {
+            _inventory.Items.Add(new Inventory.Item());
+        }
+    }
 }
